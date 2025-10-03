2c569ee8266c8ac125bb1ca9b066ab47ce9dd75d
jasayosep <jasayosep@gmail.com>
2025-10-03 11:19:22 +0700

    Update poc.js

--- poc.js
+++ poc.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     // Ambil semua elemen dengan class css-19n97h2
     const elements = document.querySelectorAll(".css-19n97h2");
 
-    // Pasang event listener click di setiap elemen
     elements.forEach(function (el) {
-        el.addEventListener("click", function () {
-            alert("Elemen dengan class css-19n97h2 diklik!");
+        el.addEventListener("mouseenter", function () {
+            alert("Mouse hover di elemen dengan class css-19n97h2!");
         });
     });
 });
